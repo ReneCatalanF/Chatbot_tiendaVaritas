38ea846fdbe5bbc9ce96bdbc8d476b78b84a8a9d
RENE CATALAN <reene.ddyyfmen@gmail.com>
2025-01-29 18:32:04 +0100

    Agregando config para variables de entorno

--- index.js
+++ index.js
@@ -10,18 +10,28 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 3000;
 
-// Configuracion del bot de Telegram
+// Configuración del bot de Telegram
 const token = process.env.TELEGRAM_BOT_TOKEN;
 const telegramBot = new TelegramBot(token);
 
-// Configuracion del agente de Dialogflow
-process.env.GOOGLE_APPLICATION_CREDENTIALS = process.env.DIALOGFLOW_JSON_KEY_FILE;
+// Configuración del agente de Dialogflow
+process.env.GOOGLE_APPLICATION_CREDENTIALS = {
+  type: process.env.DIALOGFLOW_TYPE,
+  project_id: process.env.DIALOGFLOW_PROJECT_ID,
+  private_key_id: process.env.DIALOGFLOW_PRIVATE_KEY_ID,
+  private_key: process.env.DIALOGFLOW_PRIVATE_KEY.replace(/\\n/g, '\n'), // Reemplaza \n con saltos de línea reales
+  client_email: process.env.DIALOGFLOW_CLIENT_EMAIL,
+  client_id: process.env.DIALOGFLOW_CLIENT_ID,
+  auth_uri: process.env.DIALOGFLOW_AUTH_URI,
+  token_uri: process.env.DIALOGFLOW_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.DIALOGFLOW_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.DIALOGFLOW_CLIENT_X509_CERT_URL,
+};
 
 app.post('/webhook', (req, res) => {
   const agent = new WebhookClient({ request: req, response: res });
 
   function dialogflowFulfillment(agent) {
-    // Simplemente reenvia la respuesta de Dialogflow a Telegram
     agent.setResponse(agent.originalRequest.body.queryResult.fulfillmentMessages);
   }
 
