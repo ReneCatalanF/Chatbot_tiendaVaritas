7a93483a3828ccafa29cf7a163fa25a14b9d6642
RENE CATALAN <reene.ddyyfmen@gmail.com>
2025-01-29 19:17:13 +0100

    Agregando logs

--- index.js
+++ index.js
@@ -11,15 +11,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const port = process.env.PORT || 3000;
 
 // Configuración del bot de Telegram
+console.log('Configurando bot de Telegram...');
 const token = process.env.TELEGRAM_BOT_TOKEN;
+if (!token) {
+  console.error('Error: Variable de entorno TELEGRAM_BOT_TOKEN no definida.');
+  process.exit(1);
+}
 const telegramBot = new TelegramBot(token);
+console.log('Bot de Telegram configurado.');
 
 // Configuración del agente de Dialogflow
+console.log('Configurando agente de Dialogflow...');
+if (!process.env.DIALOGFLOW_TYPE || !process.env.DIALOGFLOW_PROJECT_ID || !process.env.DIALOGFLOW_PRIVATE_KEY_ID || !process.env.DIALOGFLOW_PRIVATE_KEY || !process.env.DIALOGFLOW_CLIENT_EMAIL || !process.env.DIALOGFLOW_CLIENT_ID || !process.env.DIALOGFLOW_AUTH_URI || !process.env.DIALOGFLOW_TOKEN_URI || !process.env.DIALOGFLOW_AUTH_PROVIDER_X509_CERT_URL || !process.env.DIALOGFLOW_CLIENT_X509_CERT_URL) {
+  console.error('Error: Variables de entorno de Dialogflow no definidas.');
+  process.exit(1);
+}
 process.env.GOOGLE_APPLICATION_CREDENTIALS = {
   type: process.env.DIALOGFLOW_TYPE,
   project_id: process.env.DIALOGFLOW_PROJECT_ID,
   private_key_id: process.env.DIALOGFLOW_PRIVATE_KEY_ID,
-  private_key: process.env.DIALOGFLOW_PRIVATE_KEY.replace(/\\n/g, '\n'), // Reemplaza \n con saltos de línea reales
+  private_key: process.env.DIALOGFLOW_PRIVATE_KEY.replace(/\\n/g, '\n'),
   client_email: process.env.DIALOGFLOW_CLIENT_EMAIL,
   client_id: process.env.DIALOGFLOW_CLIENT_ID,
   auth_uri: process.env.DIALOGFLOW_AUTH_URI,
@@ -27,12 +38,19 @@ process.env.GOOGLE_APPLICATION_CREDENTIALS = {
   auth_provider_x509_cert_url: process.env.DIALOGFLOW_AUTH_PROVIDER_X509_CERT_URL,
   client_x509_cert_url: process.env.DIALOGFLOW_CLIENT_X509_CERT_URL,
 };
+console.log('Agente de Dialogflow configurado.');
 
 app.post('/webhook', (req, res) => {
+  console.log('Solicitud recibida en /webhook:', req.body);
+
   const agent = new WebhookClient({ request: req, response: res });
 
   function dialogflowFulfillment(agent) {
+    console.log('Intent detectado:', agent.intent);
+
     agent.setResponse(agent.originalRequest.body.queryResult.fulfillmentMessages);
+
+    console.log('Respuesta enviada:', agent.response);
   }
 
   agent.handleRequest(dialogflowFulfillment);
@@ -40,6 +58,7 @@ app.post('/webhook', (req, res) => {
 
 // Ruta para verificar el bot de Telegram (opcional)
 app.get('/telegram-verification', (req, res) => {
+  console.log('Solicitud GET en /telegram-verification');
   res.send('¡Bot de Telegram verificado!');
 });
 
