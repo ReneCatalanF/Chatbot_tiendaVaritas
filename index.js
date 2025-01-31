47366b1280f0a501ba9d50eb0eaf6d44347cb199
RENE CATALAN <reene.ddyyfmen@gmail.com>
2025-01-31 14:11:54 +0100

    funcion busqueda v5, extendiendo lista

--- index.js
+++ index.js
@@ -6,36 +6,244 @@ const app = express();
 app.use(bodyParser.json());
 
 const varitas = {
-  "Harry Potter": { // Clave modificada para facilitar la búsqueda
+  "Harry Potter": {
     dueño: "Harry Potter",
     madera: "Acebo",
-    nucleo: "Pluma de Fénix",
-    longitud: "11 pulgadas"
+    nucleo: "Pluma de fénix",
+    longitud: "11 pulgadas",
+    descripcion: "Varita elegida por Harry en Ollivanders, conocida por su lealtad y su capacidad para realizar hechizos poderosos."
   },
-  "Hermione Granger": { // Clave modificada para facilitar la búsqueda
-    dueño: "Hermione Granger",
-    madera: "Vid",
-    nucleo: "Fibra de corazón de dragón",
-    longitud: "10 3/4 pulgadas"
+  "Lord Voldemort": {
+    dueño: "Lord Voldemort",
+    madera: "Tejo",
+    nucleo: "Pluma de fénix",
+    longitud: "13 1/2 pulgadas",
+    descripcion: "Una varita oscura y poderosa, capaz de realizar hechizos malignos. Hermana de la varita de Harry."
   },
-  "Ron Weasley": { // Clave modificada para facilitar la búsqueda
-    dueño: "Ron Weasley",
-    madera: "Fresno",
-    nucleo: "Pelo de unicornio",
-    longitud: "12 pulgadas"
-  },
-  "Albus Dumbledore": { // Clave modificada para facilitar la búsqueda
+  "Albus Dumbledore": {
     dueño: "Albus Dumbledore",
     madera: "Sauco",
     nucleo: "Pelo de cola de Thestral",
-    longitud: "15 pulgadas"
+    longitud: "15 pulgadas",
+    descripcion: "Una de las Reliquias de la Muerte, conocida por su poder y sabiduría."
   },
-  "Lord Voldemort": { // Clave modificada para facilitar la búsqueda
-    dueño: "Lord Voldemort",
-    madera: "Tejo",
-    nucleo: "Pluma de Fénix",
-    longitud: "13 1/2 pulgadas"
-  }
+  "Hermione Granger": {
+    dueño: "Hermione Granger",
+    madera: "Vid",
+    nucleo: "Fibra de corazón de dragón",
+    longitud: "10 3/4 pulgadas",
+    descripcion: "Una varita elegante y precisa, ideal para hechizos complejos."
+  },
+  "Ron Weasley": {
+    dueño: "Ron Weasley",
+    madera: "Fresno",
+    nucleo: "Pelo de unicornio",
+    longitud: "12 pulgadas",
+    descripcion: "Una varita leal y confiable, aunque a veces un poco caprichosa."
+  },
+  "Draco Malfoy": {
+    dueño: "Draco Malfoy",
+    madera: "Boj",
+    nucleo: "Corazón de basilisco",
+    longitud: "10 pulgadas",
+    descripcion: "Una varita oscura y arrogante, reflejando la personalidad de su dueño."
+  },
+  "Luna Lovegood": {
+    dueño: "Luna Lovegood",
+    madera: "Laurel",
+    nucleo: "Vela de corazón de tentáculo de kraken",
+    longitud: "13 pulgadas",
+    descripcion: "Una varita excéntrica y única, capaz de realizar hechizos inusuales."
+  },
+  "Neville Longbottom": {
+    dueño: "Neville Longbottom",
+    madera: "Haya",
+    nucleo: "Cola de unicornio",
+    longitud: "14 pulgadas",
+    descripcion: "Una varita leal y confiable, que crece en poder junto con su dueño."
+  },
+  "Sirius Black": {
+    dueño: "Sirius Black",
+    madera: "Roble",
+    nucleo: "Cola de fénix",
+    longitud: "12 pulgadas",
+    descripcion: "Una varita poderosa y versátil, utilizada por un animago experto."
+  },
+  "Minerva McGonagall": {
+    dueño: "Minerva McGonagall",
+    madera: "Roble",
+    nucleo: "Corazón de dragón",
+    longitud: "13 pulgadas",
+    descripcion: "Una varita poderosa y versátil, ideal para profesores de Transformaciones."
+  },
+  "Remus Lupin": {
+    dueño: "Remus Lupin",
+    madera: "Fresno",
+    nucleo: "Pelo de unicornio",
+    longitud: "11 pulgadas",
+    descripcion: "Una varita leal y confiable, que acompaña a su dueño en sus aventuras."
+  },
+  "Bellatrix Lestrange": {
+    dueño: "Bellatrix Lestrange",
+    madera: "Ebony",
+    nucleo: "Vena de serpiente",
+    longitud: "12.5 pulgadas",
+    descripcion: "Una varita oscura y poderosa, utilizada para realizar hechizos crueles y tortuosos."
+  },
+  "Severus Snape": {
+    dueño: "Severus Snape",
+    madera: "Ciprés",
+    nucleo: "Cola de fénix",
+    longitud: "11 pulgadas",
+    descripcion: "Una varita elegante y letal, capaz de realizar hechizos complejos y precisos."
+  },
+  "Rubeus Hagrid": {
+    dueño: "Rubeus Hagrid",
+    madera: "Roble",
+    nucleo: "Pelo de unicornio",
+    longitud: "16 pulgadas",
+    descripcion: "Una varita robusta y poderosa, ideal para tareas que requieren fuerza bruta."
+  },
+  "Ginny Weasley": {
+    dueño: "Ginny Weasley",
+    madera: "Yew",
+    nucleo: "Corazón de dragón",
+    longitud: "11 pulgadas",
+    descripcion: "Una varita poderosa y versátil, que refleja la valentía y determinación de su dueña."
+  },
+  "Charlie Weasley": {
+    dueño: "Charlie Weasley",
+    madera: "Roble",
+    nucleo: "Cola de dragón",
+    longitud: "14 pulgadas",
+    descripcion: "Una varita resistente y duradera, ideal para trabajos de campo y criaturas mágicas."
+  },
+  "Bill Weasley": {
+    dueño: "Bill Weasley",
+    madera: "Haya",
+    nucleo: "Pelo de unicornio",
+    longitud: "13 pulgadas",
+    descripcion: "Una varita elegante y versátil, utilizada para una variedad de hechizos."
+  },
+  "Fred y George Weasley": {
+    dueños: "Fred y George Weasley",
+    madera: "Nogal",
+    núcleo: "Cola de fénix",
+    longitud: "12 pulgadas",
+    descripcion: "Un par de varitas idénticas, utilizadas para crear bromas y artilugios mágicos."
+  },
+  "Cho Chang": {
+    dueño: "Cho Chang",
+    madera: "Cerezo",
+    núcleo: "Corazón de dragón",
+    longitud: "11 pulgadas",
+    descripcion: "Una varita elegante y ligera, ideal para hechizos de vuelo y defensa."
+  },
+  "Lucius Malfoy": {
+    dueño: "Lucius Malfoy",
+    madera: "Ebony",
+    núcleo: "Vena de serpiente",
+    longitud: "13 pulgadas",
+    descripcion: "Una varita oscura y elegante, que refleja el estatus social de su dueño."
+  },
+  "Nymphadora Tonks": {
+    dueño: "Nymphadora Tonks",
+    madera: "Sauce llorón",
+    núcleo: "Vela de corazón de mantícora",
+    longitud: "12 pulgadas",
+    descripcion: "Una varita versátil y adaptable, que refleja la naturaleza cambiante de su dueña."
+  },
+  "Oliver Wood": {
+    dueño: "Oliver Wood",
+    madera: "Fresno",
+    núcleo: "Cola de unicornio",
+    longitud: "14 pulgadas",
+    descripcion: "Una varita resistente y confiable, ideal para deportes mágicos."
+  },
+  "Cornelius Fudge": {
+    dueño: "Cornelius Fudge",
+    madera: "Roble",
+    núcleo: "Corazón de dragón",
+    longitud: "13 pulgadas",
+    descripcion: "Una varita que refleja la personalidad ambigua y oportunista de su dueño."
+  },
+  "Alastor Moody (Ojoloco)": {
+    dueño: "Alastor Moody",
+    madera: "Haya",
+    núcleo: "Pelo de unicornio",
+    longitud: "12 pulgadas",
+    descripcion: "Una varita resistente y confiable, utilizada por un Auror experimentado."
+  },
+  "Barty Crouch Jr.": {
+    dueño: "Barty Crouch Jr.",
+    madera: "Ebony",
+    núcleo: "Vena de serpiente",
+    longitud: "11 pulgadas",
+    descripcion: "Una varita oscura y poderosa, utilizada para realizar hechizos malignos."
+  },
+  "Lucius Malfoy (varita original)": {
+        dueño: "Lucius Malfoy (joven)",
+        madera: "Laurel",
+        núcleo: "Vena de dragón",
+        longitud: "12 pulgadas",
+        descripcion: "La primera varita de Lucius Malfoy, antes de obtener la de Voldemort."
+    },
+    "Dolores Umbridge": {
+        dueño: "Dolores Umbridge",
+        madera: "Avellano",
+        núcleo: "Pena de fénix",
+        longitud: "9 pulgadas",
+        descripcion: "Una varita elegante pero oscura, reflejando la personalidad cruel de su dueña."
+    },
+    "Peter Pettigrew (Colagusano)": {
+        dueño: "Peter Pettigrew",
+        madera: "Abedul",
+        núcleo: "Pelo de rata",
+        longitud: "10 pulgadas",
+        descripcion: "Una varita débil y poco confiable, reflejando la cobardía de su dueño."
+    },
+    "Quirrell": {
+        dueño: "Quirrell",
+        madera: "Olmo",
+        núcleo: "Pelo de unicornio",
+        longitud: "11 pulgadas",
+        descripcion: "Una varita sencilla, utilizada por un mago débil y cobarde."
+    },
+    "Gilderoy Lockhart": {
+        dueño: "Gilderoy Lockhart",
+        madera: "Laurel",
+        núcleo: "Plumas de pavo real",
+        longitud: "12 pulgadas",
+        descripcion: "Una varita vistosa pero ineficaz, utilizada por un mago vanidoso y mentiroso."
+    },
+    "Argus Filch": {
+        dueño: "Argus Filch",
+        madera: "Roble",
+        núcleo: "Espina de puercoespín",
+        longitud: "14 pulgadas",
+        descripcion: "Una varita robusta, utilizada para mantener el orden en Hogwarts."
+    },
+    "Madame Hooch": {
+        dueño: "Madame Hooch",
+        madera: "Fresno",
+        núcleo: "Cola de unicornio",
+        longitud: "12 pulgadas",
+        descripcion: "Una varita precisa y confiable, ideal para enseñar Quidditch."
+    },
+    "Vernon Dursley": {
+        dueño: "Vernon Dursley",
+        madera: "Roble",
+        núcleo: "Pelo de jabalí",
+        longitud: "15 pulgadas",
+        descripcion: "Una varita tosca y poco mágica, reflejando la personalidad grosera de su dueño."
+    },
+    "Petunia Dursley": {
+        dueño: "Petunia Dursley",
+        madera: "Haya",
+        núcleo: "Espina de erizo",
+        longitud: "10 pulgadas",
+        descripcion: "Una varita débil y poco mágica, utilizada para tareas domésticas."
+    },
 };
 
 function buscarVarita(nombreNormalizado, varitas) {
@@ -53,7 +261,7 @@ app.post('/webhook', (req, res) => {
 
   if (intentName === 'InformacionSobreTienda') {
     const fulfillmentResponse = {
-      fulfillmentText: 'No seas un mago sin varita. ¡Ven a nuestro caldero y encuentra la tuya! \n Somos el Caldero No Chorreante ubicados en la calle Gringotts 17 \n \n Dueño actual René Ignacio Catalán Figueroa, un Saludo!'
+      fulfillmentText: 'No seas un mago sin varita. ¡Ven a nuestro caldero y encuentra la tuya! \nSomos el Caldero No Chorreante ubicados en la calle Gringotts 17 \n\nDueño actual René Ignacio Catalán Figueroa, un Saludo!'
     };
     res.json(fulfillmentResponse);
   } else if (intentName === 'InformacionVaritasFamosas') {
@@ -74,14 +282,15 @@ app.post('/webhook', (req, res) => {
       //DUEÑO encontrado
       const varitaEncontrada = buscarVarita(inputNormalizado, varitas);
 
-      
+
       if (varitaEncontrada) {
 
         // Utilizar el contexto para personalizar la respuesta (opcional)
         let respuesta = `Información sobre la varita de ${varitaEncontrada.dueño}:\n`;
         respuesta += `Madera: ${varitaEncontrada.madera}\n`;
         respuesta += `Núcleo: ${varitaEncontrada.nucleo}\n`;
-        respuesta += `Longitud: ${varitaEncontrada.longitud}`;
+        respuesta += `Longitud: ${varitaEncontrada.longitud}\n`;
+        respuesta += `Descripción: ${varitaEncontrada.descripcion}`;
 
 
         console.log("CREAMOS STRING DUEÑO ENCONTRADO: ", respuesta);
